f95782503c27b6c0c769a5c2ee285f29d650e6e2
margie-ynm <margie.ynm@gmail.com>
2016-12-04 20:56:07 -0800

    Update task.rb

--- lib/task.rb
+++ lib/task.rb
@@ -1,5 +1,6 @@
 class Task
-  # @@all_tasks = []
+
+  # Update specs once attr_reader is added
   attr_reader(:description, :list_id)
 
   define_method(:initialize) do |attributes|
@@ -7,55 +8,51 @@ class Task
     @list_id = attributes.fetch(:list_id)
   end
 
-  # Old way of doing it
-  # define_singleton_method(:all) do
-  #   @@all_tasks
-  # end
+  # Update double equals to compare objects more intuitively
+    # rspec uses double equals for comparison
+  define_method(:==) do |other_task|
+    # Compares Task objects by description attribute
+    self.description().==(other_task.description())
+  end
 
-  # New and improved all method
+  define_method(:description) do
+    @description
+  end
+
   define_singleton_method(:all) do
-    # Contains all tasks from tasks table from DB
-    returned_tasks = DB.exec("SELECT * FROM tasks;")
-    # Empty array to store task(s)
+    # Get all of 'tasks' table from DB into array
+    db_all_tasks = DB.exec("SELECT * FROM tasks")
+    # Empty array for gathering each task description
     tasks = []
-    # Loops through returned_tasks
-    returned_tasks.each() do |task|
+    # Iterate through the tasks database array to push
+    db_all_tasks.each() do |task|
       # Takes description from each task, but not the ID
-        # Example: Mow the Lawn, ID = 1
-        # description = Mow the Lawn
+        # Example: description = Feed the Dog, ID = 1
+        # description = Feed the Dog
       description = task.fetch('description')
-    # The ID comes out of the database as a string.
+      # Fetches id and converts to integer
       list_id = task.fetch("list_id").to_i()
-      # Creates a task object and stores it in the tasks array
+      # Creates a task object with DB information, stores it in the tasks array
       tasks.push(Task.new({:description => description, :list_id => list_id}))
     end
-    # Returns tasks
+    # Returns tasks array
     tasks
   end
 
-  # Old save method
-  # define_method(:save) do
-  #   @@all_tasks.push(self)
+  # Old all method
+  # define_singleton_method(:all) do
+  #   @@all_tasks
   # end
 
   # New and improved save method
   define_method(:save) do
     # Uses PG Gem to execute the argument
       # Adds an input, description, to the description column in the tasks table
-    DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}');")
+    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', '#{@list_id}');")
   end
 
-  # define_singleton_method(:clear) do
-  #   @@all_tasks = []
+  # Old save method
+  # define_method(:save) do
+  #   @@all_tasks.push(self)
   # end
-  #
-  # define_method(:delete) do
-  #   task_index = @@all_tasks.index(self)
-  #   @@all_tasks.delete_at(task_index)
-  # end
-
-  define_method(:==) do |another_task|
-    self.description().==(another_task.description())
-  end
-
 end
